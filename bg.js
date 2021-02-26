ed30113b9739e2a58a8abdbfbac9efa7f0320967
username <euny_sky11@naver.com>
2021-02-26 16:22:30 +0900

    sex

--- bg.js
+++ bg.js
@@ -1,7 +1,49 @@
 const body = document.querySelector("body");
+const proverb = document.querySelector(".proverb");
 
 const IMG_NUMBER = 8;
 
+const PRO_ARR = [{
+    quotation:
+    "If you run into a wall, don't turn around and give up.\n Figure out how to climb it.",
+    author: "- Michael Jordan"
+  },
+  {
+      quotation:
+    "Never say never, because limits are often just an illusion.",
+    author: "- Michael Jordan"
+  },
+  {
+      quotation:
+      "I can accept failure, everyone fails at something.\n But I can't accept not trying.",
+      author: "- Michael Jordan"
+  },
+  {
+      quotation:
+      "Don't be afraid of failure. This is the way to succeed.",
+      author: "- Lebron James"
+  },
+  {
+      quotation:
+      "If you're afraid to fail, then you're probably goint to fail.",
+      author: "- Kobe Bryant"
+  },
+  {
+      quotation:
+      "Success comes after you conquer your biggest obstacles and hurdles.",
+      author: "- Stephen Curry"
+  },
+  {
+      quotation:
+      "The only person who can really motivate you is you.",
+      author: "- Shaquille O'Neal"
+  },
+  {
+      quotation:
+      "We go Norwich.",
+      author: "- Steven Humba Humba"
+  }
+];
 
 
 function paintImage(imgNumber){
@@ -9,6 +51,12 @@ function paintImage(imgNumber){
     image.src = `images/${imgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.appendChild(image);
+}   
+
+
+function paintProverb(imgNumber){
+    const randomProverb =  PRO_ARR[imgNumber];
+    proverb.innerText = `\n\n${randomProverb.quotation} \n\n${randomProverb.author}`;
 }
 
 
@@ -18,10 +66,11 @@ function genRandom(){
 }   
 
 
-function init2(){
+function init(){
     const randomNumber = genRandom(); 
     paintImage(randomNumber);
+    paintProverb(randomNumber);
 }
 
 
-init2();
+init();
